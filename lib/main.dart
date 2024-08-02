d18bc445fca68dc9b86b9d95add8897c137a777d
supriyomaji <akashmaji200680@gmail.com>
2024-08-02 21:55:59 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'package:wallpaper/wallpaper.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.dark),
+      home: wallpaper(),
+    );
+  }
+}
